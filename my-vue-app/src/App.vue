e7c5bd8058310a7062b39750d0fbac591b473ed3
Gabriele Cuccarini <gabriele.cuccarini@gmail.com>
2022-12-14 17:00:46 +0100

    Started

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import {store} from './store.js'
+import TheHeader from './components/HeaderCom.vue';
+import MainCom from './components/MainCom.vue';
+
+export default {
+  components: {TheHeader, MainCom},
+  data() {
+    return {
+      store
+    }
+  }
+}
+</script>
+
+<template>
+  <TheHeader></TheHeader>
+  <MainCom></MainCom>
+</template>
+
+<style lang="scss">
+@use './styles/general.scss'
+</style>
---
+++ my-vue-app/src/components/CardCom.vue
@@ -0,0 +1,38 @@
+<script>
+import {store} from '../store.js'
+export default {
+    props: {
+        test: {
+            type: Object,
+            required: true
+        }
+    },
+  data() {
+    return {
+       store,
+    }
+  }
+}
+</script>
+
+
+
+<template>
+  <div class="card">
+    <img :src="test.src" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">{{ test.titolo }}</h5>
+      <p class="card-text">
+      <ul>
+        <li >Cottura: {{ test.cottura }}min</li>
+      </ul>
+      </p>
+    </div>
+  </div>
+</template>
+
+
+
+<style scoped lang="scss">
+
+</style>
